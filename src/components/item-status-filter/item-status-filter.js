0d076c7bfb255386fcae412c6c3b9cb24bef09e5
Roman Maha <romanmaga3@gmail.com>
2021-09-30 21:10:48 +0300

    my first react project

---
+++ src/components/item-status-filter/item-status-filter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const filterButtons = [
+  { name: 'all', label: 'All' },
+  { name: 'active', label: 'Active' },
+  { name: 'done', label: 'Done' },
+];
+
+const ItemStatusFilter = ({ filter, onFilterChange = () => {} }) => {
+  const buttons = filterButtons.map(({ name, label }) => {
+    const isActive = name === filter;
+    const classNames = 'btn ' + (isActive ? 'btn-warning' : 'btn-outline-warning');
+
+    return (
+      <button key={name} type="button" onClick={() => onFilterChange(name)} className={classNames}>
+        {label}
+      </button>
+    );
+  });
+
+  return <div className="btn-group">{buttons}</div>;
+};
+
+export default ItemStatusFilter;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import './search-panel.css';
+
+export default class SearchPanel extends Component {
+  state = {
+    term: '',
+  };
+
+  onTermChange = (e) => {
+    const { onSearchChange = () => {} } = this.props;
+    this.setState({
+      term: e.target.value,
+    });
+
+    onSearchChange(e.target.value);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input bg-dark btn-outline-warning text-light"
+        placeholder="type to search"
+        value={this.state.term}
+        onChange={this.onTermChange}
+      />
+    );
+  }
+}
